30ee1f5d0176fb920422569881b19e48fd7554df
iamthamanic <iamthamanic@gmail.com>
2025-05-09 23:29:36 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,45 +1,28 @@
-// .eslintrc.cjs
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.override.json'],
     ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true }
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
   },
   settings: {
-    react: { version: 'detect' }
+    react: { version: "detect" },
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'security',
-    'jsdoc'
-  ],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:security/recommended'
+    // Basis-Regeln
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended"
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "react"
   ],
   rules: {
-    // enforce explicit return types on functions
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    // no more `any`
-    '@typescript-eslint/no-explicit-any': 'error',
-    // JSDoc rules
-    'jsdoc/require-jsdoc': ['error', {
-      require: {
-        FunctionDeclaration: true,
-        MethodDefinition: true,
-        ClassDeclaration: true,
-        ArrowFunctionExpression: true,
-        FunctionExpression: true
-      }
-    }],
-    'jsdoc/require-param': 'error',
-    'jsdoc/require-returns': 'error'
+    // Deine individuellen Regeln hier
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    // …
   }
-}
+};
