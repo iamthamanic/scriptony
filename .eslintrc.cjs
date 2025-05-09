37d504a5343348b805d539f349443096130dd8a2
iamthamanic <iamthamanic@gmail.com>
2025-05-09 21:29:48 +0000

    chore: switch to legacy ESLint config (.eslintrc.cjs)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
@@ -24,3 +25,47 @@ module.exports = {
     // … deine weiteren Regeln
   }
 };
+=======
+
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import security from "eslint-plugin-security";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended, 
+      ...tseslint.configs.recommended,
+      // Add security rules
+      "plugin:security/recommended"
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "security": security
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "warn",
+      "security/detect-object-injection": "warn",
+      "security/detect-non-literal-regexp": "warn",
+      "security/detect-unsafe-regex": "error",
+      "security/detect-buffer-noassert": "error",
+      "security/detect-eval-with-expression": "error"
+    },
+  }
+);
+>>>>>>> 4e2e27f (chore: switch to legacy ESLint config (.eslintrc.cjs))
